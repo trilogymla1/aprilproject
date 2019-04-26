5f3fcc97cad41dc1e4749b478d8b91bed4059579
arg <anna.gaukel@gmail.com>
2019-04-25 22:11:00 -0500

    updated apis

--- api.js
+++ api.js
@@ -6,3 +6,23 @@ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=94040,us&AP
     }).then(function(response) {
       console.log(response);
     });
+
+    var URL = "https://data.cityofchicago.org/resource/pk66-w54g.json?reservation_start_date=2019-06-29T00:00:00.000";
+
+$.ajax({
+    url: URL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
+
+
+  var restaurantURL = "https://opentable.herokuapp.com/api/restaurants?zip=60616";
+
+$.ajax({
+    url: restaurantURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  })
+  
