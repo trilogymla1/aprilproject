bab80bb215f4a38d112626f44c2102c37ecd1ffb
Laura <lmcsay@gmail.com>
2019-04-27 12:04:50 -0500

    Weather Data to the page

--- api.js
+++ api.js
@@ -1,14 +1,34 @@
 var zipcodeInput = "60616"
+var dateInput = ""
 // original url without user input
 // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=94040,us&APPID=ef9d93c0bbd0f2345d418982ddbebbb7";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipcodeInput + ",us&APPID=ef9d93c0bbd0f2345d418982ddbebbb7";
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipcodeInput + ",us&units=imperial&APPID=ef9d93c0bbd0f2345d418982ddbebbb7";
 
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
+      // console.log(response);
+      console.log(response)
+
+      var tempToday = Math.floor(response.list[0].main.temp)
+      var tempTomorrow = Math.floor(response.list[8].main.temp)
+      var tempAfter = Math.floor(response.list[16].main.temp)
+      var tempAfterThat = Math.floor(response.list[24].main.temp)
+      var tempEvenAfterThat = Math.floor(response.list[39].main.temp)
+
+      var weatherToday = response.list[0].weather[0].description
+      var weatherTomorrow = response.list[8].weather[0].description
+      var weatherAfter = response.list[16].weather[0].description
+      var weatherAfterThat = response.list[24].weather[0].description
+      var weatherEvenAfterThat = response.list[39].weather[0].description
+
+      $(".day-1-forecast").text("Today: " + tempToday + "°F" + " | " + weatherToday)
+      $(".day-2-forecast").text("Tomorrow: " + tempTomorrow + "°F" + " | " + weatherTomorrow)
+      $(".day-3-forecast").text("Temperature after: " + tempAfter + "°F" + " | " + weatherAfter)
+      $(".day-4-forecast").text("Temperature after that: " + tempAfterThat + "°F" + " | " + weatherAfterThat)
+      $(".day-5-forecast").text("Temperature even after that: " + tempEvenAfterThat + "°F" + " | " + weatherEvenAfterThat)
     });
 
 // ----------------------------------- park
