c1f8f499023b57a4bcf26ffdee4e994a05c1a795
Laura <lmcsay@gmail.com>
2019-04-27 11:14:54 -0500

    API update

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=94040,us&APPID=ef9d93c0bbd0f2345d418982ddbebbb7";
+var zipcodeInput = "60616"
+// original url without user input
+// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=94040,us&APPID=ef9d93c0bbd0f2345d418982ddbebbb7";
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipcodeInput + ",us&APPID=ef9d93c0bbd0f2345d418982ddbebbb7";
+
 
     $.ajax({
       url: queryURL,
@@ -7,7 +11,12 @@ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=94040,us&AP
       console.log(response);
     });
 
-    var URL = "https://data.cityofchicago.org/resource/pk66-w54g.json?reservation_start_date=2019-06-29T00:00:00.000";
+// ----------------------------------- park
+    // original url without user input
+    // var URL = "https://data.cityofchicago.org/resource/pk66-w54g.json?reservation_start_date=2019-06-29T00:00:00.000";
+    var dateInput = "2019-06-29";
+    
+    var URL = "https://data.cityofchicago.org/resource/pk66-w54g.json?reservation_start_date=" + dateInput + "T00:00:00.000";
 
 $.ajax({
     url: URL,
@@ -16,8 +25,16 @@ $.ajax({
     console.log(response);
   });
 
+  // .then(function(response) {
+  
+  // });
 
-  var restaurantURL = "https://opentable.herokuapp.com/api/restaurants?zip=60616";
+// -------------------------------------- restaraunt
+  // original url without user input
+  // var restaurantURL = "https://opentable.herokuapp.com/api/restaurants?zip=60616";
+
+  
+  var restaurantURL = "https://opentable.herokuapp.com/api/restaurants?zip=" + zipcodeInput;
 
 $.ajax({
     url: restaurantURL,
@@ -25,4 +42,3 @@ $.ajax({
   }).then(function (response) {
     console.log(response);
   })
-  
